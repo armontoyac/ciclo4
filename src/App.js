70bd2f3cfc2312c7520d519213dcf287512aca8b
Alfredo Montoya <alfredo.montoya@quind.io>
2021-11-25 18:04:35 -0500

    add react to project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import lorem from './img/lorem-ipsum.png';
+import NavBar from './components/NavBar';
+import Proyectlist from './components/Proyectlist';
+import ProyectlistCard from './components/Proyectlistitem';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Proyectlist/>
+      <ProyectlistCard/>
+      <Footer/>
+      
+      
+      </div>
+
+  );
+}
+
+export default App;
